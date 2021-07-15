e6e7258fbb251bf3b456d709412518362884c2f4
ambrose <ambrosemak@gmail.com>
2021-07-15 17:35:44 -0500

    For Review

--- queries.sql
+++ queries.sql
@@ -1,7 +1,20 @@
 ## Part 1: Test it with SQL
+Columns:
+id			int pk
+employer	varchar(255)
+name 		varchar(255)
+skills		varchar(255)
 
 ## Part 2: Test it with SQL
+select *
+from employer
+where location = "St. Louis city";
 
 ## Part 3: Test it with SQL
+drop table techjobs.job;
 
 ## Part 4: Test it with SQL
+Select distinct name, description 
+from skill
+left join job_skills ON skill.id = job_skills.skills_id 
+order by name ASC;
